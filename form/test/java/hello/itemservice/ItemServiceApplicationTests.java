ad6a11c149837f9952fb553611585154b30a559f
hangyeol choi <chlgksruf3414@gmail.com>
2023-11-23 19:37:14 +0900

    타임리프(스프링 통합과 폼)

---
+++ form/test/java/hello/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package hello.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
